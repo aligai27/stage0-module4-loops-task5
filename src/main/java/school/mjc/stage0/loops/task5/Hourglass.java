348209706a735f06b83ef6c96be68368091c7e3c
Aleksei <alekseisligai@gmail.com>
2023-11-28 12:49:44 +0600

    Drew with nested loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if(i > 0 &&  i <= (height - 1) / 2 && (j < i || height - 1 - j < i)) {
+                    System.out.print(" ");
+                    continue;
+                }
+                if(i < height - 1 &&  i > (height - 1) / 2 && (j < height - 1 - i || j > i)) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
     }
 }
